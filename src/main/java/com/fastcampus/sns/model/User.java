768d869501c1aab83bc03bfa4a6f6a0de1f48f08
yukudaa <leeyookoo@naver.com>
2024-02-26 22:13:47 +0900

    feat : create User

--- src/main/java/com/fastcampus/sns/model/User.java
+++ src/main/java/com/fastcampus/sns/model/User.java
@@ -1,8 +1,66 @@
 package com.fastcampus.sns.model;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    private String userName;
+import java.security.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User implements UserDetails {
+
+    private Integer id;
+    private String username;
     private String password;
+    private UserRole role;
+    private Timestamp registeredAt;
+    private Timestamp updatedAt;
+    private Timestamp removedAt;
 
+
+    public static User fromEntity(UserEntity entity) {
+        return new User(
+                entity.getId(),
+                entity.getUserName(),
+                entity.getPassword(),
+                entity.getRole(),
+                entity.getRegisteredAt(),
+                entity.getUpdatedAt(),
+                entity.getRemovedAt()
+        );
+    }
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return removedAt == null;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return removedAt == null;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return removedAt == null;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return removedAt == null;
+    }
 }
