17cb89af817ca5c7e462b42cccc3d91037670663
yukudaa <leeyookoo@naver.com>
2024-02-26 22:14:17 +0900

    feat : create UserServiceTest

--- src/test/java/com/fastcampus/sns/service/UserServiceTest.java
+++ src/test/java/com/fastcampus/sns/service/UserServiceTest.java
@@ -1,97 +1,140 @@
-package com.fastcampus.sns.service;
+package com.fast.campus.simplesns.controller;
 
-import com.fastcampus.simplesns.exception.ErrorCode;
+import com.fast.campus.simplesns.controller.request.UserJoinRequest;
+import com.fast.campus.simplesns.controller.request.UserLoginRequest;
+import com.fast.campus.simplesns.exception.ErrorCode;
 import com.fast.campus.simplesns.exception.SimpleSnsApplicationException;
-import com.fast.campus.simplesns.fixture.TestInfoFixture;
-import com.fast.campus.simplesns.fixture.UserEntityFixture;
-import com.fastcampus.sns.model;
-import com.fast.campus.simplesns.model.UserRole;
-import com.fast.campus.simplesns.model.entity.UserEntity;
-import com.fast.campus.simplesns.repository.UserEntityRepository;
-import org.junit.jupiter.api.Assertions;
+import com.fast.campus.simplesns.model.User;
+import com.fast.campus.simplesns.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-public class UserServiceTest {
+@AutoConfigureMockMvc
+public class UserControllerTest {
 
     @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     UserService userService;
 
-    @MockBean
-    UserEntityRepository userEntityRepository;
-
-    @MockBean
-    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    void 로그인이_정상동작한다() {
-        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+    @WithAnonymousUser
+    public void 회원가입() throws Exception {
+        String userName = "name";
+        String password = "password";
 
-        when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.empty());
+        when(userService.join(userName, password)).thenReturn(mock(User.class));
 
-        Assertions.assertDoesNotThrow(() -> userService.login(fixture.getUserName(), fixture.getPassword()));
-
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest("name", "password"))))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void 로그인시_유저가_존재하지_않으면_에러를_내뱉는다() {
-        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+    @WithAnonymousUser
+    public void 회원가입시_같은_아이디로_회원가입하면_에러발생() throws Exception {
+        String userName = "name";
+        String password = "password";
+        when(userService.join(userName, password)).thenThrow(new SimpleSnsApplicationException(ErrorCode.DUPLICATED_USER_NAME));
 
-        when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.empty());
-        SimpleSnsApplicationException exception = Assertions.assertThrows(SimpleSnsApplicationException.class
-                , () -> userService.login(fixture.getUserName(), fixture.getPassword()));
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest("name", "password"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.DUPLICATED_USER_NAME.getStatus().value()));
+    }
 
-        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    @Test
+    @WithAnonymousUser
+    public void 로그인() throws Exception {
+        String userName = "name";
+        String password = "password";
+
+        when(userService.login(userName, password)).thenReturn("testToken");
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest("name", "password"))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void 로그인시_회원가입한적이_없다면_에러발생() throws Exception {
+        String userName = "name";
+        String password = "password";
+
+        when(userService.login(userName, password)).thenThrow(new SimpleSnsApplicationException(ErrorCode.USER_NOT_FOUND));
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest("name", "password"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.USER_NOT_FOUND.getStatus().value()));
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void 로그인시_비밀번호가_다르면_에러발생() throws Exception {
+        String userName = "name";
+        String password = "password";
+
+        when(userService.login(userName, password)).thenThrow(new SimpleSnsApplicationException(ErrorCode.INVALID_PASSWORD));
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest("name", "password"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PASSWORD.getStatus().value()));
+    }
+
+    /**
+     * 알람
+     */
+    @Test
+    @WithMockUser
+    void 알람기능() throws Exception {
+        when(userService.alarmList(any(), any())).thenReturn(Page.empty());
+        mockMvc.perform(get("/api/v1/users/alarm")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(status().isOk());
     }
 
 
     @Test
-    void 로그인시_패스워드가_다르면_에러를_내뱉는다() {
-        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
-
-        when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.of(UserEntityFixture.get(fixture.getUserName(), "password1")));
-        when(bCryptPasswordEncoder.matches(fixture.getPassword(), "password1")).thenReturn(false);
-
-        SimpleSnsApplicationException exception = Assertions.assertThrows(SimpleSnsApplicationException.class
-                , () -> userService.login(fixture.getUserName(), fixture.getPassword()));
-
-        Assertions.assertEquals(ErrorCode.INVALID_PASSWORD, exception.getErrorCode());
-    }
-
-    @Test
-    void 회원가입이_정상동작한다() {
-        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
-
-        when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.of(UserEntityFixture.get(fixture.getUserName(), fixture.getPassword())));
-        when(bCryptPasswordEncoder.encode(fixture.getPassword())).thenReturn("password_encrypt");
-        when(userEntityRepository.save(any())).thenReturn(Optional.of(UserEntityFixture.get(fixture.getUserName(), "password_encrypt")));
-
-        Assertions.assertDoesNotThrow(() -> userService.join(fixture.getUserName(), fixture.getPassword()));
-    }
-
-
-    @Test
-    void 회원가입시_아이디가_중복되면_다르면_에러를_내뱉는다() {
-        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
-
-        when(userEntityRepository.findByUserName(fixture.getUserName()))
-                .thenReturn(Optional.of(UserEntityFixture.get(fixture.getUserName(), fixture.getPassword())));
-
-        SimpleSnsApplicationException exception = Assertions.assertThrows(SimpleSnsApplicationException.class,
-                () -> userService.join(fixture.getUserName(), fixture.getPassword()));
-
-        Assertions.assertEquals(ErrorCode.DUPLICATED_USER_NAME, exception.getErrorCode());
+    @WithAnonymousUser
+    void 알람기능시_로그인하지_않은경우() throws Exception {
+        when(userService.alarmList(any(), any())).thenReturn(Page.empty());
+        mockMvc.perform(get("/api/v1/users/alarm")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
     }
 
 }
